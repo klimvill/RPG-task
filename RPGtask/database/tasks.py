c7319b0b82fb9ff621e5b74252c0ccd3a87d2642
Petr Rudichev <petrrudichev@gmail.com>
2024-07-05 20:27:05 +0400

    Добавлены ежедневные задания. Проведён небольшой рефакторинг

--- RPGtask/database/tasks.py
+++ RPGtask/database/tasks.py
@@ -13,3 +13,47 @@ def read_tasks():
 def save_tasks(data):
 	with open(task_path, "w", encoding='utf-8') as file:
 		json.dump(data, file, ensure_ascii=False)
+
+
+def get_where(num: int, tasks) -> str | None:
+	user_tasks = tasks['user_tasks']
+	daily_tasks = tasks['daily_tasks']['tasks']
+	quests = tasks['quests']
+
+	if 0 < num <= len(user_tasks):
+		where = 'user_tasks'
+	elif len(user_tasks) < num <= len(daily_tasks):
+		where = 'daily_tasks'
+	elif len(daily_tasks) < num <= len(quests):
+		where = 'quests'
+	else:
+		return None
+	return where
+
+
+def get_information_about_task(num: int, tasks) -> tuple[str, str, list[str] | None] | None:
+	user_tasks = tasks['user_tasks']
+	daily_tasks = tasks['daily_tasks']['tasks']
+	quests = tasks['quests']
+
+	if 0 < num <= len(user_tasks):
+		where = 'user_tasks'
+		text, skills = user_tasks[num - 1]
+
+	elif len(user_tasks) < num <= len(daily_tasks) + len(user_tasks):
+		where = 'daily_tasks'
+		text, skills, done = daily_tasks[num - len(user_tasks) - 1]
+
+	elif len(daily_tasks) + len(user_tasks) < num <= len(quests) + len(daily_tasks) + len(user_tasks) - 1:
+		where = 'quests'
+		text, skills = quests[num - 1]
+
+	else:
+		return None
+	return where, text, skills
+
+
+def get_task_text(num: int, tasks) -> str | None:
+	if (_ := get_information_about_task(num, tasks)) is not None:
+		return _[1]
+	return None
--- RPGtask/utils.py
+++ RPGtask/utils.py
@@ -1,16 +1,14 @@
 from random import uniform
 
 from pyxdameraulevenshtein import damerau_levenshtein_distance
+
 from .config import MULTIPLIER_OBTAINING_GOLD
+from .database import get_information_about_task
 
 
 def skill_check(skill: str, hero_info) -> str | None:
 	"""
 	Проверка на ошибки при записи навыков
-
-	:param skill: str - навык
-	:param hero_info:
-	:return: str | None - правильная запись навыка или None
 	"""
 	skills_list = list(hero_info['skills'].keys())
 
@@ -21,36 +19,7 @@ def skill_check(skill: str, hero_info) -> str | None:
 	return None
 
 
-def get_information_about_task(num: int, tasks) -> tuple[str, list[str] | None, str | None] | None:
-	# todo: Перенести в database
-
-	user_tasks = tasks['user_tasks']
-	daily_tasks = tasks['daily_tasks']
-	quests = tasks['quests']
-
-	if 0 < num <= len(user_tasks):
-		text, skills, time = user_tasks[num - 1]
-
-	elif len(user_tasks) < num <= len(daily_tasks):
-		text, skills, time = daily_tasks[num - 1]
-
-	elif len(daily_tasks) < num <= len(quests):
-		text, skills, time = quests[num - 1]
-
-	else:
-		return None
-	return text, skills, time
-
-
-def get_task_text(num: int, tasks) -> str | None:
-	# todo: Перенести в database
-
-	if (_ := get_information_about_task(num, tasks)) is not None:
-		return _[0]
-	return None
-
-
-def receiving_awards(nums: list[int], tasks, hero_info) -> list[int | dict | list]:
+def get_rewards_user_tasks(nums: list[int], tasks, hero_info):
 	nums = set(nums)
 	hero_skills = hero_info['skills']
 
@@ -63,8 +32,8 @@ def receiving_awards(nums: list[int], tasks, hero_info) -> list[int | dict | lis
 		if (task_info := get_information_about_task(num, tasks)) is None:
 			continue
 		elif task_info[1] is None:
-			data[0] += uniform(0.01 * gold_multiplier,
-							   0.05 * gold_multiplier) * MULTIPLIER_OBTAINING_GOLD  # Если задание без навыков, то даём в два раза больше золота
+			# Если задание без навыков, то даём в два раза больше золота.
+			data[0] += uniform(0.01 * gold_multiplier, 0.05 * gold_multiplier) * MULTIPLIER_OBTAINING_GOLD
 			continue
 
 		data[0] += uniform(0.01 * gold_multiplier, 0.05 * gold_multiplier)
@@ -80,5 +49,65 @@ def receiving_awards(nums: list[int], tasks, hero_info) -> list[int | dict | lis
 			else:
 				data[1][skill] = 0
 				data[1][skill] += exp
+
+	return data
+
+
+def receiving_awards(nums: set[int], tasks, hero_info) -> list[int | dict | list]:
+	daily_tasks = tasks['daily_tasks']['tasks']
+	hero_skills = hero_info['skills']
+
+	# todo: Добавить функцию получения множителей для золота и навыков
+	gold_multiplier = 1
+
+	data = [0, {}, []]  # Золото, навыки и полученный опыт, предметы
+
+	for num in nums:
+		if (task_info := get_information_about_task(num, tasks)) is None: continue
+		where = task_info[0]
+
+		if where == 'user_tasks':
+			if task_info[2] is None:
+				data[0] += uniform(0.01 * gold_multiplier,
+								   0.05 * gold_multiplier) * MULTIPLIER_OBTAINING_GOLD  # Если задание без навыков, то даём в два раза больше золота
+				continue
+
+			data[0] += uniform(0.01 * gold_multiplier, 0.05 * gold_multiplier)
+
+		elif where == 'daily_tasks': continue
+
+		for skill in task_info[2]:
+			skill_lvl = hero_skills[skill][0]
+			if skill_lvl == 0: skill_lvl = 1
+
+			exp = uniform(0.01, 0.05) * skill_lvl
+
+			if skill in data[1]:
+				data[1][skill] += exp
+			else:
+				data[1][skill] = exp
+
+	return data
+
+
+def get_rewards_daily_tasks(daily_tasks, hero_info):
+	data = [0, {}, []]  # Золото, навыки и полученный опыт, предметы
+	hero_skills = hero_info['skills']
+
+	gold_multiplier = 1
+
+	for i in range(len(daily_tasks)):
+		skills = daily_tasks[i][1]
+		data[0] += uniform(0.01 * gold_multiplier, 0.05 * gold_multiplier)
+
+		for skill in skills:
+			skill_lvl = hero_skills[skill][0]
+			if skill_lvl == 0: skill_lvl = 1
+			exp = uniform(0.01, 0.05) * skill_lvl
+
+			if skill in data[1]:
+				data[1][skill] += exp
+			else:
+				data[1][skill] = exp
 
 	return data
